4c4af6f1a3c5ea7751e86d039408cd3fd89e64a3
SirBubbls <lucassas@live.de>
2019-10-03 07:34:34 -0700

    org mode snippets

--- install.bat
+++ install.bat
@@ -1,2 +1,4 @@
 mklink %APPDATA%\.spacemacs %~dp0\spacemacs\.spacemacs
+
 mklink /d %APPDATA%\.emacs.d\private\snippets\python-mode %~dp0\spacemacs\.emacs.d\private\snippets\python-mode
+mklink /d %APPDATA%\.emacs.d\private\snippets\org-mode %~dp0\spacemacs\.emacs.d\private\snippets\org-mode
