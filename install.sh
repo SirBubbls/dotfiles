e591b71b575f23b209c1b22a3520bffc2e37a096
SirBubbls <lucassas@live.de>
2019-10-06 11:44:22 +0200

    snippets install under linux/osx

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ ln -s "$parent_path"/spacemacs/.spacemacs ~/.spacemacs
 # personal dict
 ln -s "$parent_path"/spacemacs/personal_dict ~/.emacs.d/personal_dict
 
-
-# ln -s "$parent_path/spacemacs/.emacs.d/private/snippets/org-mode ~/.emacs.d/private/snippets/org-mode
-# ln -s "$parent_path/spacemacs/.emacs.d/private/snippets/python-mode" "~/.emacs.d/private/snippets/python-mode"
+# Removing and replacing snippets folder
+rm -r ~/.emacs.d/private/snippets
+ln -s "$parent_path"/spacemacs/.emacs.d/private/snippets ~/.emacs.d/private/snippets
