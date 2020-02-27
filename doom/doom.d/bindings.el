5c326a15e4061ea6e8217ff59d31f9a164fa3414
SirBubbls <lucassas@live.de>
2020-02-27 14:37:33 +0100

    keybindings

--- doom/doom.d/bindings.el
+++ doom/doom.d/bindings.el
@@ -1,4 +1,6 @@
 ;;; ~/.doom.d/bindings.el -*- lexical-binding: t; -*-
+(when (eq 'system-type 'darwin)
+  (setq mac-command-modifier 'meta))
 
 (map! :n "t" 'evil-insert
       :n "T" 'evil-insert-line
@@ -10,4 +12,18 @@
       :n "n" 'evil-next-line
       :n "e" 'evil-previous-line
       :n "i" 'evil-forward-char
+      :n "u" 'evil-insert
+
+      (:after company
+        :map company-active-map
+        "C-n"         #'company-select-next
+        "C-e"         #'company-select-previous
+        "C-d"         #'company-show-doc-buffer
+        )
+      :after evil-mode
+      :map evil-visual-state-map
+      :nv "n" 'evil-next-line
+      :nv "e" 'evil-previous-line
       )
+
+(define-key evil-visual-state-map "n" 'evil-next-line)
