030bb3bca2b871f33a907e57053b9ee89edc209e
SirBubbls <lucassas@live.de>
2020-03-17 01:48:58 +0100

    continue on error

--- install.sh
+++ install.sh
@@ -4,20 +4,21 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P );
 cd "$parent_path";
 
 # .spacemacs file
-rm ~/.spacemacs;
+rm ~/.spacemacs || true;
 ln -s "$parent_path"/spacemacs/.spacemacs ~/.spacemacs;
 
 # .doom files
-rm -r ~/.doom.d;
+rm -r ~/.doom.d || true;
 ln -s "$parent_path"/doom/doom.d ~/.doom.d;
 
 # personal dict
-ln -s "$parent_path"/spacemacs/personal_dict ~/.emacs.d/personal_dict;
+rm ~/.emacs.d/personal_dict || true;
+ln -s "$parent_path"/spacemacs/personal_dict ~/.emacs.d/personal_dict || true;
 
 # Removing and replacing snippets folder
-rm -r ~/.emacs.d/private/snippets;
-ln -s "$parent_path"/spacemacs/.emacs.d/private/snippets ~/.emacs.d/private/snippets;
+rm -r ~/.emacs.d/private/snippets || true;
+ln -s "$parent_path"/spacemacs/.emacs.d/private/snippets ~/.emacs.d/private/snippets || true;
 
 # Install Fish Config
-rm -r ~/.config/fish;
-ln -s "$parent_path"/fish ~/.config/fish;
+rm -r ~/.config/fish || true;
+ln -s "$parent_path"/fish ~/.config/fish || true;
