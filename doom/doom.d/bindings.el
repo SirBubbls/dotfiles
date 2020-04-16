16b83844ca0ddbc0dc867ce46f11ad7c5eb93363
Lucas Sas <sirbubbls@Lucas-Sas-Desktop-Mac.local>
2020-04-16 08:08:49 +0200

    eshell on <SPC>-T

--- doom/doom.d/bindings.el
+++ doom/doom.d/bindings.el
@@ -28,7 +28,8 @@
                 :desc "list-buffers" "b" #'ivy-switch-buffer)
 )
 (map! :leader :desc "Next Workspace" "]" (lambda! (+workspace/switch-right))
-      :leader :desc "Previous Workspace" "[" (lambda! (+workspace/switch-left)))
+      :leader :desc "Previous Workspace" "[" (lambda! (+workspace/switch-left))
+      :leader :desc "Open Eshell" "T" 'eshell)
 
 (define-key evil-visual-state-map "n" 'evil-next-line)
 (define-key evil-normal-state-map (kbd "M-/") 'comment-line)
