919e1d011a6041e3fe48d0b7c6eb94dfc81ad9d1
SirBubbls <lucassas@live.de>
2020-04-21 11:56:06 +0200

    configured langtool

--- doom/doom.d/configurations/spellcheck.el
+++ doom/doom.d/configurations/spellcheck.el
@@ -7,3 +7,14 @@
   (ispell-hunspell-add-multi-dic "en_US,de_DE")
   (setq ispell-personal-dictionary "~/.doom.d/personal_dict")
 )
+
+
+;;
+;; LangTool
+;;
+(setq langtool-mother-tongue "de")
+
+;; Default Language
+(setq langtool-default-language "de-DE")
+;; Unicode Support
+(setq langtool-java-user-arguments '("-Dfile.encoding=UTF-8"))
