33ec7737bebd7d2dfaa2f6ccb1e1cb28250fb8c0
Lucas Sas <sirbubbls@Lucas-Sas-Desktop-Mac.local>
2020-04-29 19:48:18 +0200

    c config

--- doom/doom.d/configurations/clang.el
+++ doom/doom.d/configurations/clang.el
@@ -1,8 +1,11 @@
 ;;; ~/Dropbox/Configurations/dotfiles/doom/doom.d/configurations/clang.el -*- lexical-binding: t; -*-
 
 
-(setq irony-additional-clang-options "")
-
 ;; Call this function to link OSX C header to clang completion
 (defun irony-link-macos ()
-    (shell-command-to-string "echo | clang -x c++ -v -E - 2>&1 | sed -n '/^#include </,/^End/s|^[^/]*\([^ ]*/include[^ ]*\).*$|-I\1|p' >> ~/.clang_complete"))
+  (shell-command-to-string "echo | clang -x c++ -v -E - 2>&1 | sed -n '/^#include </,/^End/s|^[^/]*\([^ ]*/include[^ ]*\).*$|-I\1|p' >> ~/.clang_complete"))
+
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
