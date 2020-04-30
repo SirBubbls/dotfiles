b779f28a901b4f4c61f5a6f8487608403f0ec9d4
Lucas Sas <sirbubbls@Lucas-Sas-Desktop-Mac.local>
2020-05-01 01:12:36 +0200

    ein bindings

--- doom/doom.d/configurations/ein-config.el
+++ doom/doom.d/configurations/ein-config.el
@@ -6,8 +6,13 @@
   :n (kbd "M-RET") 'ein:worksheet-execute-all-cells-below
   :leader (:prefix "e"
             :desc "Save Notebooks" "s" #'ein:notebook-save-notebook-command
-            :desc "Run All" "ra" #'ein:worksheet-execute-all-cellS
-            :desc "Run Below" "rr" #'ein:worksheet-execute-all-cellS
-            :desc "Run Below" "rr" #'ein:worksheet-execute-all-cellS
+            :desc "Run All" "ra" #'ein:worksheet-execute-all-cells
+            :desc "Run Below" "rn" #'ein:worksheet-execute-all-cells-below
+            :desc "Run Above" "re" #'ein:worksheet-execute-all-cells-above
+            :desc "Cell Type" "t" #'ein:worksheet-toggle-cell-type
+            :desc "Insert Below" "in" #'ein:worksheet-insert-cell-below
+            :desc "Insert Above" "ie" #'ein:worksheet-insert-cell-above
+            :desc "Merge Cell" "m" #'ein:worksheet-merge-cell
+            :desc "Merge Cell" "d" #'ein:worksheet-delete-cell
             )
   )
