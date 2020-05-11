bb68fc06b2b8ec14521a96e56d8dfff2cb9e624e
Lucas Sas <sirbubbls@Lucas-Sas-Desktop-Mac.local>
2020-05-11 18:03:11 +0200

    spellcheck only if dependencies exist

--- doom/doom.d/configurations/spellcheck.el
+++ doom/doom.d/configurations/spellcheck.el
@@ -1,20 +1,29 @@
-(with-eval-after-load "ispell"
-  (setenv "LANG" "en_US, de_DE")
-  (setq ispell-really-hunspell t)
-  (setq ispell-program-name "hunspell")
-  (setq ispell-dictionary "en_US,de_DE")
-  (ispell-set-spellchecker-params)
-  (ispell-hunspell-add-multi-dic "en_US,de_DE")
-  (setq ispell-personal-dictionary "~/.doom.d/personal_dict")
+
+
+;; ;;
+;; ;; LangTool
+;; ;;
+
+;; ;; ;; Default Language
+;; ;; ;; Unicode Support
+;; (setq langtool-java-user-arguments '("-Dfile.encoding=UTF-8"))
+
+(if (executable-find "languagetool")
+  (setq langtool-mother-tongue "de")
+  (setq langtool-default-language "de-DE")
+  (setq langtool-java-user-arguments '("-Dfile.encoding=UTF-8"))
 )
 
-
-;;
-;; LangTool
-;;
-(setq langtool-mother-tongue "de")
-
-;; Default Language
-(setq langtool-default-language "de-DE")
-;; Unicode Support
-(setq langtool-java-user-arguments '("-Dfile.encoding=UTF-8"))
+(if (and (file-exists-p "~/Library/Spelling/de_DE.aff")
+         (file-exists-p "~/Library/Spelling/en_US.aff")
+         (executable-find "hunspell"))
+  (with-eval-after-load "ispell"
+    (setenv "LANG" "en_US,de_DE")
+    (setq ispell-really-hunspell t)
+    (setq ispell-program-name "hunspell")
+    (setq ispell-dictionary "en_US,de_DE")
+    (setq ispell-personal-dictionary "~/.doom.d/personal_dict")
+    (ispell-set-spellchecker-params)
+    (ispell-hunspell-add-multi-dic "en_US,de_DE")
+    )
+  )
