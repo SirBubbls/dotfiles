27ea699b38ed0840a6b8c0bf8de91e72936d2332
SirBubbls <lucassas@live.de>
2021-05-04 16:00:15 +0200

    added MC

--- doom/doom.d/init.el
+++ doom/doom.d/init.el
@@ -63,9 +63,11 @@
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       ;; multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
+       ;;objed             ; text object editing for the innocent
+       ;;parinfer          ; turn lisp into python, sort of
+       multiple-cursors  ; editing in many places at once
        ;;rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
